c4f7bec8ab63b5dc4dbcc40cc2866c163ded04b4
Andrew <aschaff4@gmail.com>
2020-05-05 13:44:09 -0400

    Merging to base

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let roleHarvester = require("role.harvester");
 let roleUpgrader = require("role.upgrader");
 let roleBuilder = require("role.builder");
-// require("prototype.spawnManager");
+require("prototype.spawnManager");
 
 module.exports.loop = function () {
   /**
@@ -16,17 +16,15 @@ module.exports.loop = function () {
     }
   }
 
-  // // for each spawn
-  // for (let spawnName in Game.spawns) {
-  //   console.log("Spawns I control: ", spawnName);
-  //   // run spawn logic
-  //   // Game.spawns[spawnName].spawnCreepsIfNecessary();
-  // }
+  // for each spawn
+  for (let spawnName in Game.spawns) {
+    // run spawn logic
+    Game.spawns[spawnName].spawnCreepsIfNecessary();
+  }
 
   var tower = Game.getObjectById("5eb0dde217e4ca600b488c16");
   if (tower) {
     var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-    console.log("closest hostile: ", closestHostile);
     if (closestHostile) {
       tower.attack(closestHostile);
     }
@@ -36,64 +34,6 @@ module.exports.loop = function () {
     if (closestDamagedStructure && tower.store.energy > 500) {
       tower.repair(closestDamagedStructure);
     }
-  }
-
-  // TODO:
-  // Look at the curent room from memory,
-  // determine how mnay screeps can be at each source from memory
-  const CreepsInArea = Game.rooms["E29N19"].lookForAtArea(LOOK_CREEPS, 7, 38, 7, 39, true).length;
-  console.log("creeps in area: ", CreepsInArea);
-
-  const SourcesInRoom = Game.rooms["E29N19"].find(FIND_SOURCES);
-  let source = undefined;
-  if (CreepsInArea > 2) {
-    source = SourcesInRoom[1];
-  } else {
-    source = SourcesInRoom[0];
-  }
-
-  // TODO:
-  /**
-   * For now, I'm hard coding the sources for the type of woker so that the
-   * builders all go towards the accesible source so they can quickly build a tower
-   * Should eventually be changed out with `${source}`
-   */
-  var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == "harvester");
-  if (harvesters.length < 2) {
-    var newName = "Harvester" + Game.time;
-    console.log("Spawning new harvester: " + newName);
-    Game.spawns["Spawn1"].spawnCreep([WORK, WORK, CARRY, MOVE, MOVE], newName, {
-      memory: { role: "harvester", source: SourcesInRoom[0] },
-    });
-  }
-  var builders = _.filter(Game.creeps, (creep) => creep.memory.role == "builder");
-
-  if (builders.length < 1) {
-    var newName = "Builder" + Game.time;
-    console.log("Spawning new builder: " + newName);
-    Game.spawns["Spawn1"].spawnCreep([WORK, WORK, CARRY, MOVE, MOVE], newName, {
-      memory: { role: "builder", source: SourcesInRoom[1] },
-    });
-  }
-
-  var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == "upgrader");
-
-  if (upgraders.length < 1) {
-    var newName = "Upgrader" + Game.time;
-    console.log("Spawning new upgrader: " + newName);
-    Game.spawns["Spawn1"].spawnCreep([WORK, WORK, CARRY, MOVE, MOVE], newName, {
-      memory: { role: "upgrader", source: SourcesInRoom[1] },
-    });
-  }
-
-  if (Game.spawns["Spawn1"].spawning) {
-    var spawningCreep = Game.creeps[Game.spawns["Spawn1"].spawning.name];
-    Game.spawns["Spawn1"].room.visual.text(
-      "🛠️" + spawningCreep.memory.role,
-      Game.spawns["Spawn1"].pos.x + 1,
-      Game.spawns["Spawn1"].pos.y,
-      { align: "left", opacity: 0.8 }
-    );
   }
 
   for (var name in Game.creeps) {
--- prototype.spawnManager.js
+++ prototype.spawnManager.js
@@ -4,23 +4,44 @@ StructureSpawn.prototype.spawnCreepsIfNecessary = function () {
   /**
    * @type {Room}
    */
-  let room = this.room;
-  console.log('this room is: ', room):
+  const room = this.room;
+  const spawn = this.name;
 
   // find all creeps in room
   /** @type {Array.<Creep>} */
-  let creepsInRoom = room.find(FIND_MY_CREEPS);
+  const creepsInRoom = room.find(FIND_MY_CREEPS);
+
+  const CreepsInArea = room.lookForAtArea(LOOK_CREEPS, 7, 38, 7, 39, true).length;
+  const SourcesInRoom = room.find(FIND_SOURCES);
+  let source = undefined;
+  if (CreepsInArea > 2) {
+    source = SourcesInRoom[0];
+  } else {
+    source = SourcesInRoom[1];
+  }
 
   // count the number of creeps alive for each role in this room
   // _.sum will count the number of properties in Game.creeps filtered by the
   //  arrow function, which checks for the creep being a specific role
   /** @type {Object.<string, number>} */
-  // let numberOfCreeps = {};
-  // for (let role of listOfRoles) {
-  //   numberOfCreeps[role] = _.sum(creepsInRoom, (c) => c.memory.role == role);
-  // }
+  let numberOfCreeps = {};
+  for (let role of listOfRoles) {
+    numberOfCreeps[role] = _.sum(creepsInRoom, (c) => c.memory.role == role);
+    if (numberOfCreeps[`${role}`] < this.memory.minCreeps[role]) {
+      let name = `${role}${Game.time.toString().slice(-2)}`;
+      Game.spawns[spawn].spawnCreep([WORK, WORK, CARRY, MOVE, MOVE], name, {
+        memory: { role: `${role}`, source: source },
+      });
+    }
+  }
 
-  // if (numberOfCreeps["harvester"] < Memory.spawns[spawn].minCreeps.harvester) {
-  //   Harvester.test();
-  // }
+  if (Game.spawns[spawn].spawning) {
+    var spawningCreep = Game.creeps[Game.spawns[spawn].spawning.name];
+    Game.spawns[spawn].room.visual.text(
+      "🛠️" + spawningCreep.memory.role,
+      Game.spawns[spawn].pos.x + 1,
+      Game.spawns[spawn].pos.y,
+      { align: "left", opacity: 0.8 }
+    );
+  }
 };
