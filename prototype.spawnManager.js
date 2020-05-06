2b340a624dd0de38f073fefd123f4258c791de91
Andrew <aschaff4@gmail.com>
2020-05-06 14:35:05 -0400

    set up dynamic body spawning, and updated harvester logic to working/non-working

--- prototype.spawnManager.js
+++ prototype.spawnManager.js
@@ -1,47 +1,57 @@
+/* eslint-disable no-undef */
 const listOfRoles = ['harvester', 'builder', 'upgrader'];
 
 StructureSpawn.prototype.spawnCreepsIfNecessary = function () {
   /**
    * @type {Room}
    */
-  const room = this.room;
+  const { room } = this;
   const spawn = this.name;
 
   // find all creeps in room
   /** @type {Array.<Creep>} */
   const creepsInRoom = room.find(FIND_MY_CREEPS);
 
-  const CreepsInArea = room.lookForAtArea(LOOK_CREEPS, 7, 38, 7, 39, true).length;
+  const CreepsInArea = room.lookForAtArea(LOOK_CREEPS, 6, 37, 7, 39, true).length;
   const SourcesInRoom = room.find(FIND_SOURCES);
-  let source = undefined;
-  if (CreepsInArea > 2) {
+  let source;
+  if (CreepsInArea >= 2) {
     source = SourcesInRoom[0];
   } else {
     source = SourcesInRoom[1];
   }
 
+  const body = [];
+  for (
+    let i = 0;
+    i < (room.energyAvailable - 50) / 50 - ((room.energyAvailable - 50 / 50) % 4);
+    i += 4
+  ) {
+    body.push(WORK, CARRY, MOVE);
+  }
+
   // count the number of creeps alive for each role in this room
   // _.sum will count the number of properties in Game.creeps filtered by the
   //  arrow function, which checks for the creep being a specific role
-  /** @type {Object.<string, number>} */
-  let numberOfCreeps = {};
-  for (let role of listOfRoles) {
-    numberOfCreeps[role] = _.sum(creepsInRoom, (c) => c.memory.role == role);
+  /** @param role {Object.<string, number>} */
+  const numberOfCreeps = {};
+  listOfRoles.forEach((role) => {
+    numberOfCreeps[role] = _.sum(creepsInRoom, (c) => c.memory.role === role);
     if (numberOfCreeps[`${role}`] < this.memory.minCreeps[role]) {
-      let name = `${role}${Game.time.toString().slice(-2)}`;
-      Game.spawns[spawn].spawnCreep([WORK, CARRY, MOVE], name, {
-        memory: { role: `${role}`, source: source },
+      const name = `${role}${Game.time.toString().slice(-2)}`;
+      Game.spawns[spawn].spawnCreep(body, name, {
+        memory: { role: `${role}`, source },
       });
     }
-  }
+  });
 
   if (Game.spawns[spawn].spawning) {
-    var spawningCreep = Game.creeps[Game.spawns[spawn].spawning.name];
+    const spawningCreep = Game.creeps[Game.spawns[spawn].spawning.name];
     Game.spawns[spawn].room.visual.text(
-      '🛠️' + spawningCreep.memory.role,
+      `🛠️ ${spawningCreep.memory.role}`,
       Game.spawns[spawn].pos.x + 1,
       Game.spawns[spawn].pos.y,
-      { align: 'left', opacity: 0.8 }
+      { align: 'left', opacity: 0.8 },
     );
   }
 };
--- role.harvester.js
+++ role.harvester.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-undef */
 /*
  * Module code goes here. Use 'module.exports' to export things:
  * module.exports.thing = 'a thing';
@@ -7,51 +9,58 @@
  * mod.thing == 'a thing'; // true
  */
 
-let creepFn = require("creepFn");
+const roleHarvester = {
+  /**
+   * @param {Creep} creep
+   */
+  run(creep) {
+    const source = creep.memory.source.id;
+    if (creep.memory.working && creep.store[RESOURCE_ENERGY] === 0) {
+      creep.memory.working = false;
+      creep.say('🔄 harvest');
+    }
+    if (!creep.memory.working && creep.store.getFreeCapacity() === 0) {
+      creep.memory.working = true;
+      creep.say('🚧 delivery');
+    }
 
-let roleHarvester = {
-  /** @param {Creep} creep **/
-  run: function (creep) {
-    let source = creep.memory.source.id;
-    if (creep.store.getFreeCapacity() > 0) {
-      if (creep.harvest(Game.getObjectById(source)) !== OK) {
-        console.log(creep.harvest(Game.getObjectById(source)));
-        creep.moveTo(Game.getObjectById(source), {
-          visualizePathStyle: { stroke: "#ff0000" },
-        });
+    if (creep.memory.working) {
+      const PriorityTargets = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+        filter: (structure) =>
+          (structure.structureType === STRUCTURE_EXTENSION ||
+            structure.structureType === STRUCTURE_SPAWN) &&
+          structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0,
+      });
+      // console.log('priority targets: ', PriorityTargets);
+      const SecondaryTargets = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+        filter: (structure) => {
+          return (
+            structure.structureType === STRUCTURE_TOWER &&
+            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
+          );
+        },
+      });
+      if (PriorityTargets) {
+        // console.log('here');
+        // console.log('free energy: ', creep.store.getCapacity());
+        if (creep.transfer(PriorityTargets, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+          creep.moveTo(PriorityTargets, {
+            visualizePathStyle: { stroke: '#00FF00' },
+          });
+        }
+      } else if (SecondaryTargets) {
+        // console.log('her2e');
+        if (creep.transfer(SecondaryTargets, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+          creep.moveTo(SecondaryTargets, {
+            visualizePathStyle: { stroke: '#00FF00' },
+          });
+        }
       }
     } else {
-      let PriorityTargets = creep.room.find(FIND_STRUCTURES, {
-        filter: (structure) => {
-          return (
-            (structure.structureType == STRUCTURE_EXTENSION ||
-              structure.structureType == STRUCTURE_SPAWN) &&
-            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
-          );
-        },
-      });
-      let SecondaryTargets = creep.room.find(FIND_STRUCTURES, {
-        filter: (structure) => {
-          return (
-            structure.structureType == STRUCTURE_TOWER &&
-            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
-          );
-        },
-      });
-      if (PriorityTargets.length > 0) {
-        if (creep.transfer(PriorityTargets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(PriorityTargets[0], {
-            visualizePathStyle: { stroke: "#00FF00" },
-          });
-        }
-      } else if (SecondaryTargets.lengty > 0) {
-        if (creep.transfer(SecondaryTargets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(SecondaryTargets[0], {
-            visualizePathStyle: { stroke: "#00FF00" },
-          });
-        }
-      } else {
-        creep.moveTo(Game.spawns.Spawn1);
+      if (creep.harvest(Game.getObjectById(source)) !== OK) {
+        creep.moveTo(Game.getObjectById(source), {
+          visualizePathStyle: { stroke: '#ff0000' },
+        });
       }
     }
   },
