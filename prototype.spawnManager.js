d7d1e14af932516241dc11253dc26d6fa41f6924
Andrew <aschaff4@gmail.com>
2020-05-07 09:57:47 -0400

    added tower prototype, updated all object.keys iterations, fixed harvester targeting logic

--- prototype.spawnManager.js
+++ prototype.spawnManager.js
@@ -8,10 +8,12 @@ StructureSpawn.prototype.spawnCreepsIfNecessary = function () {
   const { room } = this;
   const spawn = this.name;
 
+  console.log(room);
+  console.log(spawn);
+
   // find all creeps in room
   /** @type {Array.<Creep>} */
   const creepsInRoom = room.find(FIND_MY_CREEPS);
-
   const CreepsInArea = room.lookForAtArea(LOOK_CREEPS, 6, 37, 7, 39, true).length;
   const SourcesInRoom = room.find(FIND_SOURCES);
   let source;
@@ -22,21 +24,27 @@ StructureSpawn.prototype.spawnCreepsIfNecessary = function () {
   }
 
   const body = [];
-  for (
-    let i = 0;
-    i < (room.energyAvailable - 50) / 50 - ((room.energyAvailable - 50 / 50) % 4);
-    i += 4
-  ) {
+  let creepSegments = Math.floor((room.energyAvailable - 50 / 50) / 200);
+  creepSegments = creepSegments <= 3 ? undefined : creepSegments > 4 ? 5 : creepSegments;
+  for (let i = 0; i < creepSegments; i += 1) {
     body.push(WORK, CARRY, MOVE);
   }
+  const numberOfCreeps = {};
+
+  console.log(body);
 
   // count the number of creeps alive for each role in this room
   // _.sum will count the number of properties in Game.creeps filtered by the
   //  arrow function, which checks for the creep being a specific role
   /** @param role {Object.<string, number>} */
-  const numberOfCreeps = {};
   listOfRoles.forEach((role) => {
     numberOfCreeps[role] = _.sum(creepsInRoom, (c) => c.memory.role === role);
+    if (role === 'harvest' && numberOfCreeps[`${role}`] === 0) {
+      const name = `${role}${Game.time.toString().slice(-2)}`;
+      Game.spawns[spawn].spawnCreep(body || [WORK, CARRY, MOVE], name, {
+        memory: { role: `${role}`, source },
+      });
+    }
     if (numberOfCreeps[`${role}`] < this.memory.minCreeps[role]) {
       const name = `${role}${Game.time.toString().slice(-2)}`;
       Game.spawns[spawn].spawnCreep(body, name, {
